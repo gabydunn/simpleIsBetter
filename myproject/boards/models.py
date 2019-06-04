05fa74145af5d28150728ade783686a2a989c619
Gabriella Dunn <gdunn6@nait.ca>
2019-06-04 16:00:42 -0600

    added models
    create sqlite3 db

--- myproject/boards/models.py
+++ myproject/boards/models.py
@@ -1,5 +1,21 @@
 from django.db import models
-from django.conf import settings
+from django.contrib.auth.models import User
 
 # Create your models here.
-class Board()
+class Board(models.Model): 
+    name = models.CharField(max_length=30, unique=True)
+    description = models.CharField(max_length=100)
+
+class Topic(models.Model):
+    subject = models.CharField(max_length=200)
+    last_updated = models.DateTimeField(auto_now_add=True)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE,related_name='topics')
+    starter = models.ForeignKey(User, null=True, related_name='topics', on_delete=models.CASCADE)
+
+class Post(models.Model):
+    message = models.TextField(max_length=4000)
+    topic = models.ForeignKey(Topic, related_name='posts',on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True)
+    created_by = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+    updated_by = models.ForeignKey(User, null=True, related_name='+', on_delete=models.CASCADE)
